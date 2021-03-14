e1bdfe739ca69453181c4b29bdea61a1dcf9703f
kelzerock <kelzzzer@gmail.com>
2021-03-14 10:13:04 +0300

    init: start brackets-task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = [];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    arr.push(bracketsConfig[i].join(''))
+  }
+  while (arr.some(item => {
+    if (str.includes(item)) { return true } else { return false }
+  }) === true)
+    {
+      for (let x = 0; x < arr.length; x++) {
+        str = str.replace(arr[x], '')
+      }
+    }
+
+if(str === ''){return true} else {return false}
 }
